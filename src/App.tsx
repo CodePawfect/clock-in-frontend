4d41e77e12a54653edc79fc4a2b5727e5f31d930
CodePawfect <codepawfect@gmail.com>
2025-02-23 20:23:31 +0100

    add routes for login

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import './App.css'
 import LoginForm from "./features/authentication/components/LoginForm.tsx";
+import { Routes, Route, Navigate } from "react-router-dom";
+import {useSelector} from "react-redux";
+import {RootState} from "./store/store.ts";
 
 function App() {
+    const user = useSelector((state: RootState) => state.auth.user);
 
-  return (
-
-      <div>
-         <LoginForm/>
-      </div>
-  )
+    return (
+        <Routes>
+            <Route path="/login" element={user ? <Navigate to="/" /> : <LoginForm />} />
+            <Route path="/" element={user ? <h1>Home Page</h1> : <Navigate to="/login" />} />
+            {/* Catch-all redirect */}
+            <Route path="*" element={<Navigate to={user ? "/" : "/login"} />} />
+        </Routes>
+    );
 }
 
 export default App
