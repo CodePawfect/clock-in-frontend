a176707d0296f9f5cc7d263173186472ee06c40a
CodePawfect <codepawfect@gmail.com>
2025-03-31 20:17:03 +0200

    feat: add AuthProvider and wrap it around RouterProvider
    - wip: handleLogin
    - wip: handleLogout

--- src/features/auth/components/AuthProvider.tsx
+++ src/features/auth/components/AuthProvider.tsx
@@ -1,5 +1,48 @@
-export default function AuthProvider() {
-  // TODO: useContext if user is logged in
+import { createContext, PropsWithChildren, useContext } from 'react';
 
-  return <div>Auth Provider</div>;
+type AuthContext = {
+  handleLogin: () => Promise<void>;
+  handleLogout: () => Promise<void>;
+};
+
+type AuthProviderProps = PropsWithChildren;
+
+const AuthContext = createContext<AuthContext | undefined>(undefined);
+
+/**
+ * AuthProvider component that provides authentication context to its children.
+ * @param children - The child components that will have access to the authentication context.
+ */
+export default function AuthProvider({ children }: AuthProviderProps) {
+  async function handleLogin() {
+    //TODO: Implement a real login with http only cookie
+  }
+
+  async function handleLogout() {
+    //TODO: Implement a real logout with clearing the http only cookie
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        handleLogin: handleLogin,
+        handleLogout: handleLogout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+/**
+ * Custom hook to use the AuthContext
+ */
+export function useAuth() {
+  const context = useContext(AuthContext);
+
+  if (context === undefined) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+
+  return context;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import { routes } from './routes.tsx';
+import AuthProvider from './features/auth/components/AuthProvider.tsx';
 
 const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>
 );
