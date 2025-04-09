41996b7a9c81c762f9c4a48baac06842c0725763
CodePawfect <codepawfect@gmail.com>
2025-04-09 07:31:15 +0200

    test: add first unit test for useLoginMutation hook

---
+++ src/features/auth/api/mutations/useLoginMutation.test.tsx
@@ -0,0 +1,56 @@
+import { beforeEach, describe, expect, test, vi } from 'vitest';
+import createFetchMock from 'vitest-fetch-mock';
+import { renderHook, waitFor } from '@testing-library/react';
+import { LoginCredentials, useLoginMutation } from './useLoginMutation.ts';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactNode } from 'react';
+
+const fetchMocker = createFetchMock(vi);
+
+describe('test useLoginMutation hook', () => {
+  beforeEach(() => {
+    fetchMocker.resetMocks();
+  });
+
+  test('should send correct POST request', async () => {
+    const credentials: LoginCredentials = {
+      username: 'testuser',
+      password: 'testpassword',
+    };
+
+    const expectedUrl = 'http://localhost:3000/api/login';
+    const expectedMethod = 'POST';
+    const expectedBody = JSON.stringify(credentials);
+
+    fetchMock.mockResponseOnce(JSON.stringify({}), { status: 200 });
+
+    const { result } = renderHook(() => useLoginMutation(), {
+        wrapper: createQueryClientProviderWrapper(),
+    });
+
+    result.current.mutate(credentials);
+
+    await waitFor(() => {
+      expect(fetchMock).toHaveBeenCalledTimes(1);
+    });
+
+    expect(fetchMock.requests()[0].url).toEqual(expectedUrl);
+    expect(fetchMock.requests()[0].method).toEqual(expectedMethod);
+
+    const requestBody = await fetchMock.requests()[0].text();
+    expect(requestBody).toEqual(expectedBody);
+  });
+});
+
+function createQueryClientProviderWrapper() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
+    },
+  });
+  return ({ children }: { children: ReactNode }) => (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
