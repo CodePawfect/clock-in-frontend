e8b0e2dca3603c6deef887ad7b312a5410e1e88c
CodePawfect <codepawfect@gmail.com>
2025-04-09 07:30:54 +0200

    feat: setup vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config'; // Make sure you import from 'vitest/config' for test options
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
+// https://vitest.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true, // Optional: enables global APIs like describe, it, expect without importing
+    environment: 'jsdom', // Essential for React Testing Library & mocking browser APIs like fetch
+    setupFiles: ['./setupVitest.js'],
+  },
 });
