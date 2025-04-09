8e1cdbdb3508990d0fe2f81af3d06320c7c9cad3
CodePawfect <codepawfect@gmail.com>
2025-04-09 07:30:32 +0200

    feat: integrate React Query Provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,17 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import { routes } from './routes.tsx';
 import AuthProvider from './features/auth/components/AuthProvider.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const router = createBrowserRouter(routes);
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
