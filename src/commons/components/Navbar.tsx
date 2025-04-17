ec492cd0d2e42f3a2bf15c5e56e4e7e83e2a70a9
CodePawfect <codepawfect@gmail.com>
2025-04-17 19:35:06 +0200

    feat: refactor Navbar to use NavLink for routing and update icon styles

--- src/commons/components/Navbar.tsx
+++ src/commons/components/Navbar.tsx
@@ -1,16 +1,34 @@
 import './Navbar.css';
-import { ClockIcon } from '@heroicons/react/24/outline';
-import { HomeIcon } from '@heroicons/react/24/outline';
-import { NewspaperIcon } from '@heroicons/react/24/outline';
-import { Cog6ToothIcon } from '@heroicons/react/24/outline';
+import { NavLink } from 'react-router-dom';
+import {
+  ClockIcon,
+  HomeIcon,
+  NewspaperIcon,
+  Cog6ToothIcon,
+} from '@heroicons/react/24/outline';
+
+const navItems = [
+  { to: '/', Icon: HomeIcon },
+  { to: '/events', Icon: NewspaperIcon },
+  { to: '/worktime', Icon: ClockIcon },
+  { to: '/settings', Icon: Cog6ToothIcon },
+];
 
 export default function Navbar() {
   return (
     <div className="navbar">
-      <NewspaperIcon className="navbar-icon" />
-      <HomeIcon className="navbar-icon" />
-      <ClockIcon className="navbar-icon" />
-      <Cog6ToothIcon className="navbar-icon" />
+      {navItems.map(({ to, Icon }) => (
+        <NavLink
+          key={to}
+          to={to}
+          end
+          className={({ isActive }) =>
+            isActive ? 'navbar-icon navbar-icon--active' : 'navbar-icon'
+          }
+        >
+          <Icon />
+        </NavLink>
+      ))}
     </div>
   );
 }
