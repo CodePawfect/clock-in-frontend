fea80d1fc0786fac0edefe5b06bca5dbc733e977
CodePawfect <codepawfect@gmail.com>
2025-04-18 07:02:05 +0200

    feat: change WorkTimeTable.tsx to WorkTimeCards.tsx

---
+++ src/features/worktimes/components/tables/WorkTimeCards.tsx
@@ -0,0 +1,50 @@
+import { useQuery } from '@tanstack/react-query';
+import { WorkTime } from './types/WorkTime.ts';
+import { CalendarWeek } from '../../../../commons/CalenderWeek.ts';
+
+/**
+ * WorkTimeTable component
+ * @param workTimes - array of work time entries
+ */
+export default function WorkTimeCards() {
+  const week = CalendarWeek.getWeek();
+  const year = new Date().getUTCFullYear();
+
+  const { isPending, isError, data, error } = useQuery({
+    queryKey: ['worktimes', week, year],
+    queryFn: () => fetchWorkTimes(week, year),
+  });
+
+  if (isPending) {
+    return <span>Loading...</span>;
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+
+  return (
+    <div>
+      {data.map((workTime) => (
+        <div className="worktime-card" key={workTime.id}>
+          <div className="worktime">{workTime.date}</div>
+          <div className="worktime">{workTime.hours}</div>
+          <div className="worktime">{workTime.note}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+async function fetchWorkTimes(week: number, year: number): Promise<WorkTime[]> {
+  const res = await fetch(
+    `http://localhost:8080/api/worktimes/${week}/${year}`,
+    { credentials: 'include' }
+  );
+
+  if (!res.ok) throw new Error('Network response was not ok');
+
+  const { workTimes } = (await res.json()) as { workTimes: WorkTime[] };
+
+  return workTimes;
+}
--- src/pages/WorkTimePage.tsx
+++ src/pages/WorkTimePage.tsx
@@ -1,11 +1,13 @@
 import HeaderSection from '../features/worktimes/components/header/HeaderSection.tsx';
 import Navbar from '../commons/components/Navbar.tsx';
+import WorkTimeCards from '../features/worktimes/components/tables/WorkTimeCards.tsx';
 
 /** serves as composition layer and arranging components to create the final view for work times */
 export default function WorkTimePage() {
   return (
     <>
       <HeaderSection />
+      <WorkTimeCards />
       <Navbar />
     </>
   );
