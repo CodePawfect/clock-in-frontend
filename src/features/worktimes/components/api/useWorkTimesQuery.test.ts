806f4d65424f85cc0b6c204e0c785996fe56e1b8
CodePawfect <codepawfect@gmail.com>
2025-04-20 08:31:02 +0200

    refactor: format code

--- src/features/worktimes/components/api/useWorkTimesQuery.test.ts
+++ src/features/worktimes/components/api/useWorkTimesQuery.test.ts
@@ -1,43 +1,40 @@
-import {beforeEach, describe, expect, test, vi} from "vitest";
-import createFetchMock from "vitest-fetch-mock";
-import {renderHook, waitFor} from "@testing-library/react";
-import createQueryClientProviderWrapper from "../../../../test-utils/QueryClientProviderWrapper.tsx";
-import {useWorkTimesQuery} from "./useWorkTimesQuery.ts";
+import { beforeEach, describe, expect, test, vi } from 'vitest';
+import createFetchMock from 'vitest-fetch-mock';
+import { renderHook, waitFor } from '@testing-library/react';
+import createQueryClientProviderWrapper from '../../../../test-utils/QueryClientProviderWrapper.tsx';
+import { useWorkTimesQuery } from './useWorkTimesQuery.ts';
 
 const fetchMocker = createFetchMock(vi);
 
-describe("test useWorkTimesQuery hook", () => {
+describe('test useWorkTimesQuery hook', () => {
+  beforeEach(() => {
+    fetchMocker.resetMocks();
+  });
 
-    beforeEach(() => {
-        fetchMocker.resetMocks();
+  test('should success', async () => {
+    const expectedUrl = 'http://localhost:3000/api/worktimes/15/2025';
+    const expectedMethod = 'GET';
+    const week = 15;
+    const year = 2025;
+
+    fetchMock.mockResponseOnce(JSON.stringify({ workTimes: [] }), {
+      status: 200,
     });
 
-    test("should success", async () => {
-        const expectedUrl = 'http://localhost:3000/api/worktimes/15/2025';
-        const expectedMethod = 'GET';
-        const week = 15;
-        const year = 2025;
+    const { result } = renderHook(() => useWorkTimesQuery(week, year), {
+      wrapper: createQueryClientProviderWrapper(),
+    });
 
-        fetchMock.mockResponseOnce(
-            JSON.stringify({ workTimes: [] }),
-            { status: 200 }
-        )
+    await waitFor(() => {
+      expect(fetchMock).toHaveBeenCalledTimes(1);
+    });
+    await waitFor(() => {
+      expect(result.current.isSuccess).toBe(true);
+    });
 
-        const { result } = renderHook(() => useWorkTimesQuery(week, year), {
-            wrapper: createQueryClientProviderWrapper(),
-        });
-
-        await waitFor(() => {
-            expect(fetchMock).toHaveBeenCalledTimes(1);
-        });
-        await waitFor(() => {
-            expect(result.current.isSuccess).toBe(true)
-        })
-
-        expect(fetchMock.requests()[0].url).toEqual(expectedUrl);
-        expect(fetchMock.requests()[0].method).toEqual(expectedMethod);
-        expect(result.current.isError).toBe(false)
-        expect(result.current.data).toEqual([])
-    })
-
-})
+    expect(fetchMock.requests()[0].url).toEqual(expectedUrl);
+    expect(fetchMock.requests()[0].method).toEqual(expectedMethod);
+    expect(result.current.isError).toBe(false);
+    expect(result.current.data).toEqual([]);
+  });
+});
